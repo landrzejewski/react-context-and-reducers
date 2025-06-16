7955842a958eac12782b494973cae4df26a6b175
Admin <landrzejewski.poczta@gmail.com>
2025-06-16 15:45:13 +0200

    context

--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -1,5 +1,5 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react';
-import { createPortal } from 'react-dom';
+import {forwardRef, useImperativeHandle, useRef} from 'react';
+import {createPortal} from 'react-dom';
 import Cart from './Cart';
 
 /*
@@ -17,30 +17,27 @@ createPortal - createPortal renders the modal outside the normal React component
 #modal). This is useful for modals, tooltips, dropdowns, etc., which need to break out of layout constraints like overflow: hidden.
  */
 
-const CartModal = forwardRef(function Modal(
-  { cartItems, onUpdateCartItemQuantity, title, actions },
-  ref
-) {
-  const dialog = useRef();
+const CartModal = forwardRef(function Modal({title, actions}, ref) {
+    const dialog = useRef();
 
-  useImperativeHandle(ref, () => {
-    return {
-      open: () => {
-        dialog.current.showModal();
-      },
-    };
-  });
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialog.current.showModal();
+            },
+        };
+    });
 
-  return createPortal(
-    <dialog id="modal" ref={dialog}>
-      <h2>{title}</h2>
-      <Cart items={cartItems} onUpdateItemQuantity={onUpdateCartItemQuantity} />
-      <form method="dialog" id="modal-actions">
-        {actions}
-      </form>
-    </dialog>,
-    document.getElementById('modal')
-  );
+    return createPortal(
+        <dialog id="modal" ref={dialog}>
+            <h2>{title}</h2>
+            <Cart/>
+            <form method="dialog" id="modal-actions">
+                {actions}
+            </form>
+        </dialog>,
+        document.getElementById('modal')
+    );
 });
 
 export default CartModal;
--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -2,7 +2,6 @@ export default function Shop({ children }) {
   return (
     <section id="shop">
       <h2>Delicious Orange-Inspired Foods</h2>
-
       <ul id="products">{children}</ul>
     </section>
   );
